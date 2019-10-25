0fce69f1803b01d2eddd0f0ad73eb2509a2a611d
ntuckovic <ntuckovic@gmail.com>
2019-10-25 07:37:53 +0200

    initial model fields

--- src/dandy/content/models.py
+++ src/dandy/content/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=511)
+    label = models.CharField(verbose_name=_('Label'), max_length=255,
+                             default='', blank=True)
+    lead_text = models.TextField(verbose_name=_(
+        'Lead text'), default='', blank=True)
+    content = models.TextField(verbose_name=_(
+        'Content'), default='', blank=True)
