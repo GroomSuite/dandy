32fe8ce63b33b6fad11c6531ccbce6582bba3008
ntuckovic <ntuckovic@gmail.com>
2019-11-04 07:32:28 +0100

    make detail image preview

--- src/dandy/content/admin.py
+++ src/dandy/content/admin.py
@@ -16,16 +16,20 @@ admin.site.register(Article, ArticleAdmin)
 
 class ImageAdmin(admin.ModelAdmin):
 
-    def image_preview(self, obj):
+    def image_preview(self, obj, max_width='512px'):
         if obj.image_file:
-            return mark_safe(f'<img src="{settings.MEDIA_URL}{obj.image_file}" style="width: 100%; max-width: 256px;" />')
+            return mark_safe(f'<img src="{settings.MEDIA_URL}{obj.image_file}" style="width: 100%; max-width: {max_width};" />')
         else:
             return 'No Image Found'
+
+    def list_image_preview(self, obj):
+        return self.image_preview(obj, '256px')
 
     image_preview.allow_tags = True
 
     list_display = ['calculated_name',
-                    'created_on', 'image_file', 'image_preview']
+                    'created_on', 'image_file', 'list_image_preview']
+    readonly_fields = ('image_preview',)
 
 
 admin.site.register(Image, ImageAdmin)
