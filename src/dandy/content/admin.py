bc2b3fe3ef4abec307ee37300256880542aa7fb2
ntuckovic <ntuckovic@gmail.com>
2019-11-07 07:17:07 +0100

    filters and keywords configured

--- src/dandy/content/admin.py
+++ src/dandy/content/admin.py
@@ -6,30 +6,57 @@ from .models import Image, Article, Keyword
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'is_published',
-                    'publish_from', 'created_on', 'last_change']
-    raw_id_fields = ['main_image', 'alter_image']
+    list_display = [
+        "title",
+        "is_published",
+        "publish_from",
+        "created_on",
+        "last_change",
+    ]
+    list_filter = ["is_published"]
+    raw_id_fields = ["main_image", "alter_image", "keywords"]
+    search_fields = ["title", "label", "lead_text"]
+    readonly_fields = ["created_on"]
 
 
 admin.site.register(Article, ArticleAdmin)
 
 
 class ImageAdmin(admin.ModelAdmin):
-
-    def image_preview(self, obj, max_width='512px'):
+    def image_preview(self, obj, max_width="512px"):
         if obj.image_file:
-            return mark_safe(f'<img src="{settings.MEDIA_URL}{obj.image_file}" style="width: 100%; max-width: {max_width};" />')
+            return mark_safe(
+                f'<img src="{settings.MEDIA_URL}{obj.image_file}" style="width: 100%; max-width: {max_width};" />'
+            )
         else:
-            return 'No Image Found'
+            return "No Image Found"
 
     def list_image_preview(self, obj):
-        return self.image_preview(obj, '256px')
+        return self.image_preview(obj, "256px")
 
     image_preview.allow_tags = True
 
-    list_display = ['calculated_name',
-                    'created_on', 'image_file', 'list_image_preview']
-    readonly_fields = ('image_preview',)
+    list_display = ["calculated_name", "created_on", "image_file", "list_image_preview"]
+    search_fields = ["title", "image_file"]
+
+    readonly_fields = ["image_preview", "created_on"]
+    fields = [
+        "image_file",
+        "image_preview",
+        "title",
+        "description",
+        "data",
+        "created_on",
+    ]
 
 
 admin.site.register(Image, ImageAdmin)
+
+
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_on"]
+    search_fields = ["name"]
+    readonly_fields = ["created_on"]
+
+
+admin.site.register(Keyword, KeywordAdmin)
--- src/dandy/content/models.py
+++ src/dandy/content/models.py
@@ -6,16 +6,19 @@ from .mixins import CreatedDateMixin
 
 
 class Image(CreatedDateMixin):
-    image_file = models.ImageField(verbose_name=_(
-        'Image'), max_length=500, upload_to='upload')
-    title = models.CharField(verbose_name=_('Title'), blank=True,
-                             null=True, max_length=350)
-    description = models.TextField(verbose_name=_(
-        'Description'), default='', blank=True)
+    image_file = models.ImageField(
+        verbose_name=_("Image"), max_length=500, upload_to="upload"
+    )
+    title = models.CharField(
+        verbose_name=_("Title"), blank=True, null=True, max_length=350
+    )
+    description = models.TextField(
+        verbose_name=_("Description"), default="", blank=True
+    )
     data = JSONField(default=dict, blank=True, null=True)
 
     def __str__(self):
-        return self.calculated_name
+        return f"{self.id}:{self.calculated_name}"
 
     @property
     def calculated_name(self):
@@ -24,32 +27,56 @@ class Image(CreatedDateMixin):
 
 class Keyword(CreatedDateMixin):
     name = models.CharField(
-        verbose_name=_('Name'), max_length=100, db_index=True, unique=True)
+        verbose_name=_("Name"), max_length=100, db_index=True, unique=True
+    )
     data = JSONField(default=dict, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id}:{self.name}"
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+
+        return super().save(*args, **kwargs)
 
 
 class Article(CreatedDateMixin):
-    title = models.CharField(verbose_name=_('Title'), max_length=511)
-    label = models.CharField(verbose_name=_('Label'), max_length=255,
-                             default='', blank=True)
+    title = models.CharField(verbose_name=_("Title"), max_length=511)
+    label = models.CharField(
+        verbose_name=_("Label"), max_length=255, default="", blank=True
+    )
     lead_text = models.TextField(verbose_name=_(
-        'Lead text'), default='', blank=True)
+        "Lead text"), default="", blank=True)
+    main_image = models.ForeignKey(
+        "content.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="image",
+    )
+    alter_image = models.ForeignKey(
+        "content.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="alter_image",
+    )
     content = models.TextField(verbose_name=_(
-        'Content'), default='', blank=True)
-    main_image = models.ForeignKey('content.Image', null=True, blank=True,
-                                   on_delete=models.SET_NULL, related_name='image')
-    alter_image = models.ForeignKey('content.Image', null=True, blank=True,
-                                    on_delete=models.SET_NULL, related_name='alter_image')
-    publish_from = models.DateTimeField(verbose_name=_(
-        'Publish from'), null=True, blank=True, db_index=True)
+        "Content"), default="", blank=True)
+    keywords = models.ManyToManyField(
+        "content.Keyword", verbose_name=_("Keywords"))
+    publish_from = models.DateTimeField(
+        verbose_name=_("Publish from"), null=True, blank=True, db_index=True
+    )
     publish_to = models.DateTimeField(
-        verbose_name=_('Publish to'), null=True, blank=True)
+        verbose_name=_("Publish to"), null=True, blank=True
+    )
     is_published = models.BooleanField(
-        verbose_name=_('Is published'), default=False)
+        verbose_name=_("Is published"), default=False)
     data = JSONField(default=dict, blank=True, null=True)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ["-created_on"]
 
     def __str__(self):
-        return f'{self.id}:{self.title}'
+        return f"{self.id}:{self.title}"
