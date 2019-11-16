1a498c15bb52c9aa701f1be1110184872bebe5cd
ntuckovic <ntuckovic@gmail.com>
2019-11-16 17:12:03 +0100

    adding dynamic rest and fixing serializers

--- src/dandy/content/serializers.py
+++ src/dandy/content/serializers.py
@@ -9,26 +9,34 @@ class ImageSerializer(DynamicModelSerializer):
     class Meta:
         model = Image
         name = "image"
-        fields = ("id", "image_file", "title", "url")
+        fields = [
+            "id", "image_file", "title", "url",
+            "data"
+        ]
+        deferred_fields = ["data"]
 
 
 class KeywordSerializer(DynamicModelSerializer):
     class Meta:
         model = Keyword
         name = "keyword"
-        fields = ("id", "name", "url")
+        fields = [
+            "id", "name", "url",
+            "data"
+        ]
+        deferred_fields = ["data"]
 
 
 class ArticleSerializer(DynamicModelSerializer):
     class Meta:
         model = Article
         name = "article"
-        fields = (
+        fields = [
             "id", "label", "title", "publish_from",
             "main_image", "alter_image", "lead_text",
             "keywords",
             "url"
-        )
+        ]
 
     main_image = DynamicRelationField('ImageSerializer', embed=True)
     alter_image = DynamicRelationField('ImageSerializer', embed=True)
