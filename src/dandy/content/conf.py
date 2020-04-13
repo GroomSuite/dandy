3d5b581d64a670173bd3a7b150bce76f1ec696e3
ntuckovic <ntuckovic@gmail.com>
2020-04-13 17:57:28 +0200

    add default types, upgrade default schema option

--- src/dandy/content/conf.py
+++ src/dandy/content/conf.py
@@ -3,9 +3,30 @@ from appconf import AppConf
 
 
 class ContentConf(AppConf):
-    DEFAULT_ARTICLE_DATA = {
-        'content': [],
-        'keywords': [],
-        'cover_image': None,
-        'section': None
+    ARTICLE_SCHEMA = {
+        'content': {
+            'type': 'list'
+        },
+        'keywords': {
+            'type': 'many',
+            'model': 'content.Keyword'
+        },
+        'cover_image': {
+            'type': 'one',
+            'model': 'content.Image'
+        },
+        'section': {
+            'type': 'one',
+            'model': 'content.Section'
+        }
+    }
+    TYPE_DEFAULTS = {
+        'string': '',
+        'int': 0,
+        'float': 0.0,
+        'datetime': None,
+        'list': [],
+        'dict': {},
+        'one': None,
+        'many': []
     }
--- src/dandy/content/utils.py
+++ src/dandy/content/utils.py
@@ -2,4 +2,14 @@ from .conf import settings
 
 
 def get_default_article_data():
-    return settings.CONTENT_DEFAULT_ARTICLE_DATA
+    schema_definition = settings.CONTENT_ARTICLE_SCHEMA
+    type_defaults = settings.CONTENT_TYPE_DEFAULTS
+
+    default_data = {}
+
+    for key, field_definition in schema_definition.items():
+        field_type = field_definition.get('type', 'string')
+
+        default_data[key] = type_defaults.get(field_type, '')
+
+    return default_data
