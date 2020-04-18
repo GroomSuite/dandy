d4e3655e4b5482a72dbb8dd82b1b9926a8353c76
ntuckovic <ntuckovic@gmail.com>
2020-04-18 18:35:12 +0200

    workon object serialization

--- src/dandy/content/signals.py
+++ src/dandy/content/signals.py
@@ -1,11 +1,19 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
-from .conf import settings
-
+# TODO: move this somewhere else (module?)
 from django.apps import apps
 
-from .exceptions import ModelDoesNotExistError, UnsuportedRelationTypeError
+from .conf import settings
+from .exceptions import (
+    ModelDoesNotExistError,
+    UnsuportedRelationTypeError,
+    ModelInstanceDoesNotExistError,
+    IdValueNotFoundError
+)
+from .serializers import serializers_mapping
+
+# TODO: move this somewhere else (module?)
 
 
 def validate_model_name(name, definition):
@@ -18,6 +26,7 @@ def validate_model_name(name, definition):
             f'Cannot find model "{model_name}" for field "{name}"')
 
 
+# TODO: move this somewhere else (module?)
 def validate_relation_type(name, definition):
     relation_types = settings.CONTENT_RELATION_TYPES
     field_relation_type = definition.get('type')
@@ -28,6 +37,7 @@ def validate_relation_type(name, definition):
         )
 
 
+# TODO: move this somewhere else (module?)
 def get_relation_definitions():
     schema_definition = settings.CONTENT_ARTICLE_SCHEMA
 
@@ -46,15 +56,32 @@ def get_relation_definitions():
     return relation_definitions
 
 
+# TODO: move logic inside somewhere else (module?)
 @receiver(pre_save, sender='content.Article')
 def serialize_related_objects(sender, instance, **kwargs):
     relation_definitions = get_relation_definitions()
 
     for key, definition in relation_definitions.items():
         model_class = apps.get_model(definition.get('model'))
+        serializer_class = serializers_mapping.get(model_class)
         value = instance.data.get(key)
         default_value = settings.CONTENT_TYPE_DEFAULTS.get(
             definition.get('type'))
 
         if value != default_value:
-            print(model_class.objects.filter(id=value).first())
+            if isinstance(value, int):
+                related_id = value
+            elif isinstance(value, dict) and 'id' in value:
+                related_id = value['id']
+            else:
+                raise IdValueNotFoundError
+
+            related_obj = model_class.objects.filter(id=related_id).first()
+
+            if related_obj:
+                serialized_data = serializer_class(
+                    related_obj, context={'request': None}, include_fields='*').data
+
+                instance.data[key] = serialized_data
+            else:
+                raise ModelInstanceDoesNotExistError
